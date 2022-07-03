1952a16dd7df55fc7025aa81a15568552c8e0bd5
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-07-04 00:54:56 +0300

    refactor: exception folder has been removed

---
+++ src/Core/DapperORM.Application/Validations/Create/AddCategoryValidator.cs
@@ -0,0 +1,18 @@
+﻿using DapperORM.Domain.Constants;
+using DapperORM.Domain.Entities;
+using FluentValidation;
+
+namespace DapperORM.Application.Validations.Create
+{
+    public class AddCategoryValidator:AbstractValidator<Category>
+    {
+        public AddCategoryValidator()
+        {
+            RuleFor(p => p.Description)
+                .NotEmpty()
+                .MinimumLength(3)
+                .MaximumLength(250)
+                .WithMessage(ValidationMessages.Category_Description_Length_Error);
+        }
+    }
+}
