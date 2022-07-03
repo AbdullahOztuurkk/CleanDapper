8d6c1c0e76e4bf7a30ceaef78fd82584bdd2b2da
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-07-04 00:53:42 +0300

    feat: added some rules in add product validator

--- src/Core/DapperORM.Application/Validations/Create/AddProductValidator.cs
+++ src/Core/DapperORM.Application/Validations/Create/AddProductValidator.cs
@@ -25,6 +25,15 @@ namespace DapperORM.Application.Validations.Create
                 .GreaterThan(0)
                 .WithMessage(ValidationMessages.Product_Quantity_Must_Greater_Than_Zero);
 
+            RuleFor(p => p.UnitPrice)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(0)
+                .WithMessage(ValidationMessages.Product_Price_Must_Greater_Than_Or_Equal_To_Zero);
+
+            RuleFor(p => p.UnitsInStock)
+                .NotEmpty()
+                .GreaterThan(0)
+                .WithMessage(ValidationMessages.Product_Stock_Must_Greater_Than_Zero);
         }
     }
 }
--- src/Core/DapperORM.Domain/Constants/ValidationMessages.cs
+++ src/Core/DapperORM.Domain/Constants/ValidationMessages.cs
@@ -10,6 +10,8 @@ namespace DapperORM.Domain.Constants
     {
         public const string Product_Name_Length_Error = "Product name's length must between 3 and 70 characters!";
         public const string Product_Color_Must_be_Known_Color = "Product color must be any known color!";
-        public const string Product_Quantity_Must_Greater_Than_Zero = "Product quantity must greater than zero!";    
+        public const string Product_Quantity_Must_Greater_Than_Zero = "Product quantity must greater than zero!";
+        public const string Product_Price_Must_Greater_Than_Or_Equal_To_Zero = "product price must be greater than or equal to 0!";
+        public const string Product_Stock_Must_Greater_Than_Zero = "Product stock must greater than zero!";
     }
 }
