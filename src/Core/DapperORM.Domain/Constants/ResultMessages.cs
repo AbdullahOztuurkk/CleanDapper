1bbc440c38c060824a2fbd0f75734086d3cd4fa3
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-07-05 23:15:51 +0300

    feat: added result messages as constant to domain layer

---
+++ src/Core/DapperORM.Domain/Constants/ResultMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace DapperORM.Domain.Constants
+{
+    public class ResultMessages
+    {
+        public const string Product_Added     = "Product has been added!";
+        public const string Product_Updated   = "Product has been updated!";
+        public const string Product_Deleted   = "Product has been removed";
+
+        public const string Category_Added    = "Category has been added!";
+        public const string Category_Updated  = "Category has been updated!";
+        public const string Category_Deleted  = "Category has been removed";
+    }
+}
