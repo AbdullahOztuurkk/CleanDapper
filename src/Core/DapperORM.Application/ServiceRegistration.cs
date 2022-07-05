209e6cabd8a9d5fe98c92cc31deb47b73ba502fd
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-07-05 23:17:03 +0300

    feat: extension method has been created

--- src/Core/DapperORM.Application/ServiceRegistration.cs
+++ src/Core/DapperORM.Application/ServiceRegistration.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace DapperORM.Persistence
 {
-    public class ServiceRegistration
+    public static class ServiceRegistration
     {
+        public static void AddApplicationDependencies(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        }
     }
 }
