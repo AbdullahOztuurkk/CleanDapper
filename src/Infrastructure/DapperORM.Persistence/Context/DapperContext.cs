90442fb9edc86e8956f6b915552604b1231f2699
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-07-07 01:18:17 +0300

    feat: dapper context class has been created

---
+++ src/Infrastructure/DapperORM.Persistence/Context/DapperContext.cs
@@ -0,0 +1,30 @@
+﻿using DapperORM.Application.Interfaces.DapperContext;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DapperORM.Persistence.Context
+{
+    public class DapperContext : IDapperContext
+    {
+        IConfiguration configuration;
+        public DapperContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public void Execute(Action<SqlConnection> @event)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                @event(connection);
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
