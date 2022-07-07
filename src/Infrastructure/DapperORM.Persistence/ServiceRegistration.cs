2a2ba46454c056f578ccfe698ced3080d8f5037e
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-07-07 15:46:06 +0300

    feat: extension method has been created in persistence layer

--- src/Infrastructure/DapperORM.Persistence/ServiceRegistration.cs
+++ src/Infrastructure/DapperORM.Persistence/ServiceRegistration.cs
@@ -1,6 +1,18 @@
-﻿namespace DapperORM.Persistence
+﻿using DapperORM.Application.Interfaces.DapperContext;
+using DapperORM.Application.Interfaces.Repositories;
+using DapperORM.Persistence.Context;
+using DapperORM.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DapperORM.Persistence
 {
-    public class ServiceRegistration
+    public static class ServiceRegistration
     {
+        public static void AddPersistenceDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, DapperProductRepository>();
+            services.AddScoped<ICategoryRepository, DapperCategoryRepository>();
+            services.AddScoped<IDapperContext, DapperContext>();
+        }
     }
 }
