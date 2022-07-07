81d2679dea869cc413893a2be6f7b86fb55d8996
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-07-07 15:29:05 +0300

    feat: category query handlers has been created

---
+++ src/Core/DapperORM.Application/Queries/GetAllCategoryQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using DapperORM.Application.Interfaces.Repositories;
+using DapperORM.Domain.Common.Result;
+using DapperORM.Domain.Entities;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DapperORM.Application.Queries
+{
+    public class GetAllCategoryQuery : IRequest<IDataResult<List<Category>>> { }
+    public class GetAllCategoryQueryHandler : IRequestHandler<GetAllCategoryQuery, IDataResult<List<Category>>>
+    {
+        ICategoryRepository categoryRepository;
+        public GetAllCategoryQueryHandler(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        public Task<IDataResult<List<Category>>> Handle(GetAllCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var result = categoryRepository.GetAll();
+            return Task.FromResult<IDataResult<List<Category>>>(new SuccessDataResult<List<Category>>(result));
+        }
+    }
+    
+}
---
+++ src/Core/DapperORM.Application/Queries/GetCategoryQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using DapperORM.Application.Interfaces.Repositories;
+using DapperORM.Domain.Common.Result;
+using DapperORM.Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DapperORM.Application.Queries
+{
+    public class GetCategoryQuery : IRequest<IDataResult<Category>>
+    {
+        public int Id { get; set; }
+    }
+
+    public class GetCategoryQueryHandler : IRequestHandler<GetCategoryQuery, IDataResult<Category>>
+    {
+        ICategoryRepository categoryRepository;
+        public GetCategoryQueryHandler(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+        public Task<IDataResult<Category>> Handle(GetCategoryQuery request, CancellationToken cancellationToken)
+        {
+            var result = categoryRepository.Get(request.Id);
+            return Task.FromResult<IDataResult<Category>>(new SuccessDataResult<Category>(result));
+        }
+    }
+}
