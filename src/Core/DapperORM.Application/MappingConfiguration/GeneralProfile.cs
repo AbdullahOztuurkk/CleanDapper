3c4d9374396004eabaa8c50c6f4cda874687a4c9
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-07-09 03:41:28 +0300

    feat: added profile for mapping

---
+++ src/Core/DapperORM.Application/MappingConfiguration/GeneralProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using DapperORM.Application.Commands;
+using DapperORM.Application.Queries;
+using DapperORM.Domain.Entities;
+
+namespace DapperORM.Application.MappingConfiguration
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            //Commands
+            CreateMap<Product, CreateProductCommandRequest>().ReverseMap();
+            CreateMap<Product, DeleteProductCommandRequest>().ReverseMap();
+            CreateMap<Category, CreateCategoryCommandRequest>().ReverseMap();
+            CreateMap<Category, DeleteCategoryCommandRequest>().ReverseMap();
+
+            //Queries
+            CreateMap<Product, GetProductQueryRequest>().ReverseMap();
+            CreateMap<Product, GetAllProductQueryRequest>().ReverseMap();
+            CreateMap<Category, GetCategoryQueryRequest>().ReverseMap();
+            CreateMap<Category, GetAllCategoryQueryRequest>().ReverseMap();
+        }
+    }
+}
