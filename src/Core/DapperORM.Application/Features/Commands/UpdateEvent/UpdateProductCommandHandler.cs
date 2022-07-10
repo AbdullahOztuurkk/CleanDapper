349abb713b78d7bc55e3eb1f4fcbed5e66f0e0ce
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-07-10 17:08:43 +0300

    feat: created validator tool and used it before any operation

---
+++ src/Core/DapperORM.Application/Features/Commands/UpdateEvent/UpdateProductCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using DapperORM.Application.Interfaces.Repositories;
+using DapperORM.Application.Validations.Common;
+using DapperORM.Application.Validations.Update;
+using DapperORM.Domain.Common.Result;
+using DapperORM.Domain.Constants;
+using DapperORM.Domain.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DapperORM.Application.Features.Commands.UpdateEvent
+{
+    public class UpdateProductCommandRequest : IRequest<IResult>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int QuantityPerUnit { get; set; }
+        public int UnitPrice { get; set; }
+        public string Color { get; set; }
+        public int UnitsInStock { get; set; }
+
+    }
+
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommandRequest,IResult>
+    {
+        private readonly IProductRepository productRepository;
+        private readonly UpdateProductValidator validator;
+        private readonly IMapper mapper;
+        public UpdateProductCommandHandler(IProductRepository productRepository, IMapper mapper, UpdateProductValidator validator)
+        {
+            this.productRepository = productRepository;
+            this.mapper = mapper;
+            this.validator = validator;
+        }
+
+        public Task<IResult> Handle(UpdateProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            var product = mapper.Map<Product>(request);
+            EntityValidator.Validate(validator, product);
+            productRepository.Update(product);
+            return Task.FromResult<IResult>(new SuccessResult(ResultMessages.Product_Updated));
+        }
+    }
+}
---
+++ src/Core/DapperORM.Application/Validations/Common/EntityValidator.cs
@@ -0,0 +1,17 @@
+﻿using DapperORM.Domain.Common.Result;
+using FluentValidation;
+using System.Threading.Tasks;
+
+namespace DapperORM.Application.Validations.Common
+{
+    public static class EntityValidator
+    {
+        public static Task<IResult> Validate<T>(IValidator<T> validator, T value)
+        {
+            var result = validator.Validate(value);
+            if (result.Errors.Count > 0)
+                return Task.FromResult<IResult>(new ErrorResult(result.Errors.ToString()));
+            return null;
+        }
+    }
+}
