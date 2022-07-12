0e3c744c34a11cd3a5dad2eee46fcbe58f19c3fd
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-07-12 17:40:13 +0300

    feat: namespaces has been updated in mapping profile

--- src/Core/DapperORM.Application/MappingConfiguration/GeneralProfile.cs
+++ src/Core/DapperORM.Application/MappingConfiguration/GeneralProfile.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
-using DapperORM.Application.Commands;
-using DapperORM.Application.Queries;
+using DapperORM.Application.Features.Commands.CreateEvent;
+using DapperORM.Application.Features.Commands.DeleteEvent;
+using DapperORM.Application.Features.Commands.UpdateEvent;
+using DapperORM.Application.Features.Queries.GetAllEvent;
+using DapperORM.Application.Features.Queries.GetEvent;
 using DapperORM.Domain.Entities;
 
 namespace DapperORM.Application.MappingConfiguration
@@ -12,12 +15,15 @@ namespace DapperORM.Application.MappingConfiguration
             //Commands
             CreateMap<Product, CreateProductCommandRequest>().ReverseMap();
             CreateMap<Product, DeleteProductCommandRequest>().ReverseMap();
+            CreateMap<Product, UpdateProductCommandRequest>().ReverseMap();
             CreateMap<Category, CreateCategoryCommandRequest>().ReverseMap();
             CreateMap<Category, DeleteCategoryCommandRequest>().ReverseMap();
+            CreateMap<Category, UpdateCategoryCommandRequest>().ReverseMap();
 
             //Queries
             CreateMap<Product, GetProductQueryRequest>().ReverseMap();
             CreateMap<Product, GetAllProductQueryRequest>().ReverseMap();
+            CreateMap<Product, GetProductByCategoryQueryRequest>().ReverseMap();
             CreateMap<Category, GetCategoryQueryRequest>().ReverseMap();
             CreateMap<Category, GetAllCategoryQueryRequest>().ReverseMap();
         }
